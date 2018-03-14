eb75258dc0b797982fb9fcff86bff3e449f50a05
Dennis Muensterer <dennismuensterer@aol.com>
2018-03-14 02:51:47 +0100

    Finalized example with label to edit and usable buttons

--- AwesomeGithub/ViewController.swift
+++ AwesomeGithub/ViewController.swift
@@ -30,6 +30,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 
 }
